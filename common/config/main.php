124fee7f90fd717397f80d2f4f4ecf11f46f8ec8
Timur <web.develop@mail.ru>
2016-01-03 00:42:18 +0300

    Add configure

--- common/config/main.php
+++ common/config/main.php
@@ -1,6 +1,9 @@
 <?php
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'name' => 'Терем',
+    'language' => 'ru',
+    'timeZone' => 'Europe/Moscow',
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
